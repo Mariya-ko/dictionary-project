9a4ab24c85b73ea3c2d64b5c36cca28a1b0a67d2
Mariya-ko <106267195+Mariya-ko@users.noreply.github.com>
2022-09-03 21:52:09 +0300

    Added Search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 
 import './App.css';
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header"></header>
-      Hello
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Mariya-ko</small>
+        </footer>
+    </div>
     </div>
   );
 }
 
-export default App;
+
